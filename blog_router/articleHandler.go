2d090ca4036dc6e5d0de5171093b3596e0b82e22
kaito071831 <kaito071831@gmail.com>
2022-06-04 08:31:06 +0900

    Add comment

--- blog_router/articleHandler.go
+++ blog_router/articleHandler.go
@@ -8,16 +8,19 @@ import (
 	"github.com/kaito071831/go_blog/utility"
 )
 
+// 記事の型
 type Article struct {
 	Id int `gorm:"type:int AUTO_INCREMENT"`
 	Title string
 	Body string
 }
 
+// データベースを自動的にマイグレーションする
 func init(){
 	utility.Db.Set("gorm:table_options", "ENGINE = InnoDB").AutoMigrate(&Article{})
 }
 
+// 記事の一覧を表示
 func Index(c *gin.Context) {
 	articlelist := []Article{}
 	utility.Db.Find(&articlelist)
@@ -26,10 +29,12 @@ func Index(c *gin.Context) {
 	})
 }
 
+// 記事作成フォームを表示
 func New(c *gin.Context) {
 	c.HTML(http.StatusOK, "article/new.html", nil)
 }
 
+// 記事を作成する
 func Create(c *gin.Context) {
 	if err := c.Request.ParseForm(); err != nil {
 		log.Fatalf("フォームの送信に失敗しました: %v", err)
--- blog_router/staticHandler.go
+++ blog_router/staticHandler.go
@@ -6,6 +6,7 @@ import(
 	"github.com/gin-gonic/gin"
 )
 
+// トップページを表示
 func TopHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "static/index.html", gin.H{
 		"title": "Hello World",
